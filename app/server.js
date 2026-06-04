a249844d4cfcfe921cfbccbd4960aace0bc2a246
Cypedium <aurell.mikael@gmail.com>
2026-06-04 11:16:45 +0200

    added more monboDB updates

--- app/server.js
+++ app/server.js
@@ -1,12 +1,14 @@
 // server.js
 const app = require('../app/app');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const MONGO_URI = 'mongodb://localhost:27017/feedbackApp';
+const MONGO_URI = process.env.MONGO_URI //|| 'mongodb://localhost:27017/aurellfeedback';
 
 // 🔗 Connect to MongoDB
 mongoose.connect(MONGO_URI)
   .then(() => {
-    app.listen(4000, "0.0.0.0", () => console.log('✅ Backend running on port 4000'));
+    app.listen(4000, "0.0.0.0", () => 
+      console.log('✅ Backend running on port 4000'));
   })
   .catch((err) => console.error('❌ MongoDB connection error:', err));
